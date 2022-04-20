88543c45ce320a57702ec328a1a4d61b931b2e2f
nineteen57 <prospaul999@gmail.com>
2022-04-20 12:24:03 -0700

    First Commit In a While

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to a paramete and updates its value.
+ * @n: input integer.
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
